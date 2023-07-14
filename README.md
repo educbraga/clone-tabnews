e1dd74cea17409a4ff6af1db5d5163a3870ce449
Eduardo Braga <eduardocbraga@hotmail.com>
2023-07-13 22:38:46 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto de estudo que visa recriar o site https://www.tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>De bacon a vida 😎</h1>
+  return <h1>De bacon a vida 😎</h1>;
 }
 
 export default Home;
